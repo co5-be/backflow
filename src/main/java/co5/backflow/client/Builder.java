efc8c939ebc542dafa10dcc61b27443f1f3c7a6d
co5-be <102328728+co5-be@users.noreply.github.com>
2022-04-02 09:43:15 -0600

    Update Builder.java

--- src/main/java/co5/backflow/client/Builder.java
+++ src/main/java/co5/backflow/client/Builder.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 import java.util.function.Function;
 
 public interface Builder<W extends WorkUnit, T extends Enum> {
+    // An ordered list of steps to perform on each request/payload 
     public ArrayList<StageDescriptor<W,T>> build();
+    //A Function object that will authorize if a request will be accepted
     public Function<String,Boolean> getAuthorizer();
+    //A Function object that will provide work units
     public Function<UUID,W> getWorkUnitFactory();
 }
